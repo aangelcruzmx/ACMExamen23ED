615b936b1b7a18b07dd1c1054d8129d86679d4a0
ranger2040 <aangelcruzmx@gmail.com>
2023-12-05 13:30:04 +0100

    Cambio al main en Restadores

--- src/AcmExamen25E.java
+++ src/AcmExamen25E.java
@@ -26,4 +26,38 @@ public class AcmExamen25E {
 
         scanner.close();
     }
+    public static double restar(double a, double b) {
+
+        return a - b;
+
+    }
+
+    public static double dividir(double a, double b) {
+
+        if (b != 0) {
+
+            return a / b;
+
+        } else {
+
+            throw new IllegalArgumentException("No se puede dividir por cero");
+
+        }
+
+    }
+
+    public static double raizCuadrada(double a) {
+
+        if (a >= 0) {
+
+            return Math.sqrt(a);
+
+        } else {
+
+            throw new IllegalArgumentException("No se puede calcular la raíz cuadrada de un número negativo");
+
+        }
+
+    }
+
 }
